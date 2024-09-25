8b10353b53a800dfbfcdb62b7a557f53775c1248
SiddAgilewit <siddhu@agilewits.com>
2024-09-25 18:41:46 +0530

    Updates

--- src/app/_components/Categories/CategoryCard/index.tsx
+++ src/app/_components/Categories/CategoryCard/index.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import Link from 'next/link'
 
-import { Category } from '../../../../payload/payload-types'
+import { Category, Media } from '../../../../payload/payload-types'
 import { useFilter } from '../../../_providers/Filter'
 
 import classes from './index.module.scss'
@@ -14,17 +14,22 @@ type CategoryCardProps = {
 const CategoryCard = ({ category }: CategoryCardProps) => {
   const media = category.media as Media
   const { setCategoryFilters } = useFilter()
+  console.log(media.url)
 
   return (
-    <Link
-      href="/products"
-      className={classes.card}
-      style={{ backgroundImage: `url(${media.url})` }}
+    <Link 
+      href="/products" 
+      className={classes.card} 
       onClick={() => setCategoryFilters([category.id])}
     >
+      {media.url ? (
+        <img src={media.url} alt={category.title} className={classes.image} />
+      ) : (
+        <div className={classes.placeholder}>No Image Available</div>
+      )}
       <p className={classes.title}>{category.title}</p>
     </Link>
   )
 }
 
-export default CategoryCard
+export default CategoryCard;
