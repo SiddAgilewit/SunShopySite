c38b9fd3f23ec7ab28418d85bf47c0267e1d2688
SiddAgilewit <siddhu@agilewits.com>
2024-09-26 00:47:28 +0530

    Deploy

--- src/payload/access/admins.ts
+++ src/payload/access/admins.ts
@@ -1,10 +1,14 @@
-import type { AccessArgs } from 'payload/config'
+import type { AccessArgs } from 'payload/config';
+import { checkRole } from '../collections/Users/checkRole';
+import type { User } from '../payload-types';
 
-import { checkRole } from '../collections/Users/checkRole'
-import type { User } from '../payload-types'
+type IsAdmin = (args: AccessArgs<unknown, User>) => boolean;
 
-type isAdmin = (args: AccessArgs<unknown, User>) => boolean
-
-export const admins: isAdmin = ({ req: { user } }) => {
-  return checkRole(['admin'], user)
-}
+export const admins: IsAdmin = ({ req: { user } }) => {
+  // Check if user is defined before checking the role
+  if (!user) {
+    return false; // Or handle the case when user is not defined
+  }
+  
+  return checkRole(['admin'], user);
+};
